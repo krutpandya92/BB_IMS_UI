9842b8028d81936fea7ec08fef457aa2516fa589
krutpandya92 <krutbpandya@gmail.com>
2022-11-20 10:43:47 +0530

    Routing related bug fixes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,23 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { PolicyListComponent } from './components/policy-list/policy-list.component';
-import { DashboardComponent } from './views/dashboard/dashboard.component';
-import { LayoutComponent } from './views/layout/layout.component';
 
 const routes: Routes = [
   {
-    path: "",
-    component: PolicyListComponent,
+    path: 'dashboard',
+    loadChildren: () =>
+      import('./views/dashboard/dashboard.module').then(
+        (m) => m.DashboardModule
+      ),
   },
-  {
-    path: "policylist",
-    component: PolicyListComponent,
-  },
-  { path: "**", redirectTo: "dashboard", pathMatch: "full" },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
